ee5cf7eeba176f2a71cd0fab505bc27c3a500346
HarishTeens <arishh2@gmail.com>
2020-10-09 19:51:08 +0000

    update contributors list

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ firebaseAdmin.initializeApp({
 const slackRef=firebaseAdmin.firestore().collection("slack");
 const repoRef=firebaseAdmin.firestore().collection("repos");
 const p5jsRef=firebaseAdmin.firestore().collection("p5js");
+const contributorsRef=firebaseAdmin.firestore().collection("contributors");
 
 let globalCount=0;
 
@@ -37,32 +38,64 @@ const isValidBranch= async (branchName,fullRepoName)=>{
         const repo=each.data();
         defaultBranches.set(repo.owner+"/"+repo.name,repo.branch);
     })
-    console.log(defaultBranches,fullRepoName,branchName);
     return defaultBranches.get(fullRepoName) == branchName;
+    
+}
+
+const updateContributors=async (commits,fullRepoName)=>{
+    let contributors=new Set();
+    commits.map((commit)=>{
+        contributors.add(commit.author.username);
+    })
+    const prevContributors=await contributorsRef.get();    
+
+    for(let contributor of contributors){
+        contributorsRef.doc(contributor).get().then(async (snapshot)=>{
+            if(snapshot.exists){
+                const contributions=(await contributorsRef.doc(contributor).get()).data().contribution;
+                const contributionsSet=new Set();
+                contributions.forEach((contribution)=>{
+                    contributionsSet.add(contribution);
+                })
+                contributionsSet.add(fullRepoName);
+                const contributionsArray=[];
+                for(let item of contributionsSet){
+                    contributionsArray.push(item);
+                }
+                await contributorsRef.doc(contributor).update({contribution: contributionsArray});
+            }else{
+                await contributorsRef.doc(contributor).set({contribution:[fullRepoName]});
+            }
+        })
+    }
+    
     
 }
 
 
 express.post("/push",async (req,res)=>{
     const repoBody=req.body;
-    
-    const branchName=repoBody.ref.split("/")[2];
-    const fullRepoName=repoBody.repository.full_name;
-    if(await isValidBranch(branchName,fullRepoName)){
-        console.log("inside");
-        const commits=repoBody.commits.length;
-        const previousCount= await (await p5jsRef.doc('p5js').get()).data().count;        
-        p5jsRef.doc('p5js').update({count:previousCount+commits});
-        res.sendStatus(202);
+    if(!repoBody.ref){
+        res.sendStatus(204);
     }else{
-        res.sendStatus(203);
-    }    
+        const branchName=repoBody.ref.split("/")[2];
+        const fullRepoName=repoBody.repository.full_name;
+        if(await isValidBranch(branchName,fullRepoName)){
+            const commits=repoBody.commits.length;
+            const previousCount= await (await p5jsRef.doc('p5js').get()).data().count;        
+            p5jsRef.doc('p5js').update({count:previousCount+commits});
+            await updateContributors(repoBody.commits,fullRepoName);
+            res.sendStatus(202);
+        }else{
+            res.sendStatus(203);
+        }    
+    }
+    
     
 })
 
 express.post("/",async (req,res)=>{
     const slackBody=req.body;    
-    console.log(slackBody);
     if(slackBody.event.type=="team_join"){
         await fetchCount();
         const userProfile=slackBody.event.user;        
@@ -83,5 +116,5 @@ express.get("/",(req,res)=>{
 })
 
 express.listen(3000,async ()=>{
-    console.log("Server Started at http://localhost:300");
+    console.log("Server Started at http://localhost:3000");
 })
